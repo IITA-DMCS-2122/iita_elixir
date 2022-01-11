009154f303944e6b8b1bd8aad1d26c8d1fe41300
TinkerGlass <btmikolajczyk@wp.pl>
2022-01-11 10:37:41 +0100

    add Scrapper module
    Add functions to download links for details of a single car

---
+++ todo/lib/todo/scrapper.ex
@@ -0,0 +1,98 @@
+defmodule Todo.Scrapper do
+  @moduledoc """
+  The Scrapper context.
+  """
+
+  @doc """
+  Returns a link list of individual cars for specified page from olx page.
+
+  ## Examples
+
+      iex> get_page_cars_links(1)
+      [%String, ...]
+
+  """
+  def get_page_cars_links(page_number) do
+    adress = if page_number === 1 do
+        "https://www.olx.pl/motoryzacja/samochody/"
+      else
+        "https://www.olx.pl/motoryzacja/samochody/?page=" <> Integer.to_string(page_number)
+    end
+    case HTTPoison.get(adress) do
+    {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+      urls =
+      body
+      |> Floki.find("h3")
+      |> Floki.find("a")
+      |> Floki.attribute("href")
+      urls
+    {:ok, %HTTPoison.Response{status_code: 404}} ->
+      IO.puts "Not found :("
+    {:error, %HTTPoison.Error{reason: reason}} ->
+      IO.inspect reason
+   end
+  end
+
+
+  @doc """
+  Returns a page number for cars from olx page
+
+  ## Examples
+
+      iex> get_pages_number()
+      21
+
+  """
+  def get_pages_number() do
+    case HTTPoison.get("https://www.olx.pl/motoryzacja/samochody/") do
+    {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+      urls =
+      body
+      |> Floki.find("span")
+      |> Floki.find("a")
+      |> Floki.find("span")
+      |> Floki.text
+      splitted_urls = String.split(urls, "...")
+      cuurent_string = List.last(splitted_urls)
+      splitted_urls = String.split(cuurent_string, "następna")
+      pages_number = List.first(splitted_urls)
+      elem(Integer.parse(pages_number), 0)
+    {:ok, %HTTPoison.Response{status_code: 404}} ->
+      IO.puts "Not found :("
+    {:error, %HTTPoison.Error{reason: reason}} ->
+      IO.inspect reason
+   end
+  end
+
+
+  @doc """
+  Returns a list of link list of individual cars from olx page.
+
+  ## Examples
+
+      iex> get_all_cars_from_all_pages()
+      [[%String, ...], ...]
+
+  """
+  def get_all_cars_from_all_pages() do
+    pages_number = Todo.Scrapper.get_pages_number()
+    for n <- 1..pages_number do
+      get_page_cars_links(n)
+    end
+  end
+
+
+  @doc """
+  Returns a link list of individual cars from olx page.
+
+  ## Examples
+
+      iex> get_result_as_one_list()
+      [[%String, ...]]
+
+  """
+  def get_result_as_one_list() do
+    all_pages = Todo.Scrapper.get_all_cars_from_all_pages()
+    List.flatten(all_pages)
+  end
+end
