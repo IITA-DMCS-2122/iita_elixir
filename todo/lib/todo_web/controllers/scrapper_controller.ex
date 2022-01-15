0661c9cb9f8add897a58e09f0db01aaa0b08d0fa
FlaszKub <244008@edu.p.lodz.pl>
2022-01-15 15:35:49 +0100

    config for sentry and add logging on scrapping process

--- todo/lib/todo_web/controllers/scrapper_controller.ex
+++ todo/lib/todo_web/controllers/scrapper_controller.ex
@@ -15,17 +15,21 @@ defmodule TodoWeb.ScrapperController do
   end
 
   def create(conn, %{"scrapp" => scrapp_params}) do
+    Sentry.capture_message("Info", extra: %{extra: "Scrapping process started"})
     scrapped_data = Scrapper.get_car_details_from_page(scrapp_params["page_number"])
     for car_params <- scrapped_data do
       if car_params[:link] not in [nil] and car_params[:make] not in [nil] and car_params[:model] not in [nil] do
         try do
             Cars.create_car(car_params)
           rescue
-            e in Ecto.ConstraintError -> IO.puts("Próba dodanie tego samego rekordu")
+            e in Ecto.ConstraintError ->
+              IO.puts("Attempt to add already exising db entry")
+              Sentry.capture_exception(e, [stacktrace: __STACKTRACE__, extra: %{extra: "Attempt to add already exising db entry"}])
           end
       end
     end
     changeset = Scrapp.changeset(%Scrapp{})
+    Sentry.capture_message("Info", extra: %{extra: "Scrapping process completed"})
     conn = put_flash(conn, :info, "Scrapping process completed")
     render(conn, "new.html", changeset: changeset)
   end
